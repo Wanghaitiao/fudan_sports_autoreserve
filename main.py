03c1b713ad45d2fb2c16864188b982311b19e18c
Wanghaitiao <112834887+Wanghaitiao@users.noreply.github.com>
2022-10-28 14:49:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import logs
 SERVICE_CATEGORY = "2c9c486e4f821a19014f82381feb0001"  # This is the category ID for "Sports Reservation". It usually doesn't change.
 
 # Fill in these data
-USER_ID = "YOUR_USER_ID"
-USER_PASSWORD = "YOUR_USER_PASSWORD"
-CAMPUS_NAME = "邯郸校区"
+USER_ID = "20110190048"
+USER_PASSWORD = "970921wf,"
+CAMPUS_NAME = "江湾校区"
 SPORT_NAME = "羽毛球"
-SPORT_LOCATION = "正大体育馆羽毛球(标场)"
-DATE = "2022-03-06"
-TIME = "10:00"
+SPORT_LOCATION = "江湾体育馆羽毛球场"
+DATE = "2022-10-31"
+TIME = "21:00"
 
 # Optional data
-EMAILS = ["YOUR_EMAIL"]  # Receive error notifications by email
-YOUR_EMAIL = "YOUR_EMAIL"  # Account to send email from
-EMAIL_PASSWORD = "YOUR_EMAIL_PASSWORD"  # Password for the email account
+EMAILS = ["1127700793@qq.com"]  # Receive error notifications by email
+YOUR_EMAIL = "1127700793@qq.com"  # Account to send email from
+EMAIL_PASSWORD = "970921wf,"  # Password for the email account
 
 
 if __name__ == '__main__':
